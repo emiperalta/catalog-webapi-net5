ac66642dc284046930126d9b99dfb4ff54fe3175
Emiliano <emilianop96@hotmail.com>
2021-08-03 17:45:11 -0300

    create item repository interface

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -5,13 +5,13 @@ using Catalog.Models;
 
 namespace Catalog.Repositories
 {
-	public class InMemItemsRepository
+	public class InMemItemsRepository : IItemsRepository
 	{
-		private readonly List<Item> items = new() 
+		private readonly List<Item> items = new()
 		{
-			new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 10, CreatedDate = DateTimeOffset.UtcNow },	
-			new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },	
-			new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 15, CreatedDate = DateTimeOffset.UtcNow },	
+			new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 10, CreatedDate = DateTimeOffset.UtcNow },
+			new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+			new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 15, CreatedDate = DateTimeOffset.UtcNow },
 		};
 
 		public IEnumerable<Item> GetItems()
