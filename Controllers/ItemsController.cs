a83c6f12112702f8b4bd0e8c04f0e0c78da9987c
Emiliano <emilianop96@hotmail.com>
2021-08-03 23:05:55 -0300

    implement the repository methods in their corresponding route

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -33,5 +33,41 @@ namespace Catalog.Controllers
             if (item == null) return NotFound();            
             return item.AsDto();
         }
+
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+        {
+            var item = new Item{ 
+                Id = Guid.NewGuid(),
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow
+            };            
+            _repository.CreateItem(item);
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = _repository.GetItem(id);
+            if(existingItem is null) return NotFound();
+            Item updatedItem = existingItem with
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+            _repository.UpdateItem(updatedItem);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var itemToDelete = _repository.GetItem(id);
+            if (itemToDelete == null) return NotFound();
+            _repository.DeleteItem(itemToDelete.Id);
+            return NoContent();
+        }
     }
 }
