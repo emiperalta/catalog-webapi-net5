e788fc8ccbd15b0b92000f92ec2db33441cf3acb
Emiliano <emilianop96@hotmail.com>
2021-08-04 21:50:31 -0300

    create filter builder. add remaining operations

--- Repositories/MongoDbItemsRepository.cs
+++ Repositories/MongoDbItemsRepository.cs
@@ -12,6 +12,7 @@ namespace Catalog.Repositories
 		private const string collectionName = "items";
 
 		private readonly IMongoCollection<Item> itemsCollection;
+		private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
 
 		public MongoDbItemsRepository(IMongoClient mongoClient)
 		{
@@ -26,22 +27,25 @@ namespace Catalog.Repositories
 
 		public void DeleteItem(Guid id)
 		{
-			throw new NotImplementedException();
+			var filter = filterBuilder.Eq(item => item.Id, id);
+			itemsCollection.DeleteOne(filter);
 		}
 
 		public Item GetItem(Guid id)
 		{
-			throw new NotImplementedException();
+			var filter = filterBuilder.Eq(item => item.Id, id);
+			return itemsCollection.Find(filter).SingleOrDefault();
 		}
 
 		public IEnumerable<Item> GetItems()
 		{
-			return itemsCollection.FindSync(new BsonDocument()).ToList();
+			return itemsCollection.Find(new BsonDocument()).ToList();
 		}
 
 		public void UpdateItem(Item item)
 		{
-			throw new NotImplementedException();
+			var filter = filterBuilder.Eq(existingItem => existingItem.Id, item.Id);
+			itemsCollection.ReplaceOne(filter, item);
 		}
 	}
 }
