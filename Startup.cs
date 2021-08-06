7c691b046d234094e64d18ff98c8f5fdfed0a87a
Emiliano <emilianop96@hotmail.com>
2021-08-06 15:20:32 -0300

    move https redirection to dev env

--- Startup.cs
+++ Startup.cs
@@ -77,9 +77,8 @@ namespace Catalog
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog v1"));
+                app.UseHttpsRedirection();
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
